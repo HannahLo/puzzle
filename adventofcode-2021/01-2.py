74dc17b9dc83d288fbf01cb63fa0e8381949d6b9
yatinglo <yatinglo@yatinglode-MacBook-Pro.local>
2021-12-02 21:02:43 +0800

    01: use index to handle group by 3

--- adventofcode-2021/01-2.py
+++ adventofcode-2021/01-2.py
@@ -5,24 +5,20 @@ input = open(sys.argv[1], 'r').read().split('\n')
 if input[-1] == '':
     input.pop()
 
-def get_grater_count(list):
-    greater_count = 0
+greater_count = 0
 
-    pre_num = sys.maxsize
-    for num_str in list:
-        num = int(num_str)
-        if num > pre_num:
-            greater_count += 1
-        pre_num = num
-
-    return greater_count
+"""
+for idx in range(3, len(input)):
+    # (int(input[idx-1]) + int(input[idx-2]) + int(input[idx-3])) < (int(input[idx]) + int(input[idx-1]) + int(input[idx-2]))
+    # int(input[idx-3]) < int(input[idx-2])
+    if int(input[idx-3]) < int(input[idx-2]):
+        greater_count+=1
+"""
 
 
-sum_list = []
+# actually, check 0 -> len-4
+for idx in range(0, len(input)-3):
+    if int(input[idx]) < int(input[idx+1]):
+        greater_count+=1
 
-for idx, val in enumerate(input):
-    if idx > 1:
-        sum = int(input[idx]) + int(input[idx-1]) + int(input[idx-2])
-        sum_list.append(sum)
-
-print(get_grater_count(sum_list))
+print(greater_count)

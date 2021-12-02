2672f958a19ffb339453847fa8ad75cea2d7d336
yatinglo <yatinglo@yatinglode-MacBook-Pro.local>
2021-12-02 20:49:11 +0800

    01: use range

--- adventofcode-2021/01-1.py
+++ adventofcode-2021/01-1.py
@@ -1,17 +1,16 @@
 import sys
 
-list = open('input.txt', 'r').read().split('\n')
+list = open(sys.argv[1], 'r').read().split('\n')
 
 if list[-1] == '':
     list.pop()
 
 greater_count = 0
 
-pre_num = sys.maxsize
-for num_str in list:
-    num = int(num_str)
+for number in range(1, len(list)):
+    num = int(list[number])
+    pre_num = int(list[number - 1])
     if num > pre_num:
         greater_count += 1
-    pre_num = num
 
 print(greater_count)
